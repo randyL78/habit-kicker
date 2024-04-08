e351609c2635f31f4a630d202654478039256d6a
Randy Layne <randydavidl78@gmail.com>
2024-04-08 07:43:50 -0400

    Fix production cache invalidation

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Habit Kicker</h1>
-      <p>Here to help you kick that habit!!!</p>
+      <p>Here to help you kick that habit!</p>
     </div>
   )
 }
