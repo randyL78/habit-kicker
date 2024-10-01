438213bf0d39ec0d1380e5ffcbf1d80a68f7a84a
Randy Layne <randydavidl78@gmail.com>
2024-10-01 12:49:29 -0400

    Add API production to pipeline

--- hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HealthController.java
+++ hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HealthController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/${base.path}")
-public class HelloController {
+@RequestMapping("${base.path}")
+public class HealthController {
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World!");
+    public ResponseEntity<String> healthcheck() {
+        return ResponseEntity.ok("healthy");
     }
 }
