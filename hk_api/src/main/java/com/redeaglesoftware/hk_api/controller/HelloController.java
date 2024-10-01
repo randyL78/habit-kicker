217e21841326b75b1c7442d026a0fdbd78c0ac04
Randy Layne <randydavidl78@gmail.com>
2024-10-01 11:28:15 -0400

    Change API mapping to account for load balancer direction. Add ECS push to pipeline

--- hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HelloController.java
+++ hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/hk-staging")
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
