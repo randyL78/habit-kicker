79d145b059c3e916e6f6be380c6f7184da6a93f9
Randy Layne <randydavidl78@gmail.com>
2024-10-01 12:21:15 -0400

    Update controller to use path based on environment

--- hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HelloController.java
+++ hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hk-staging")
+@RequestMapping("/${base.path}")
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
