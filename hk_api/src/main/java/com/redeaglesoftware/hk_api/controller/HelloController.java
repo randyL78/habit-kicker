38cf20ee2d4a4b1fd6e887e825bcb75f2ffad6a7
Randy Layne <randydavidl78@gmail.com>
2024-10-01 10:28:59 -0400

    Add hello api. Add api build and containerization steps to pipeline

---
+++ hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.redeaglesoftware.hk_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
