847a6bc9bffd603b91ee6ae77b56ee1fa08cc803
Randy Layne <randydavidl78@gmail.com>
2024-10-01 13:00:57 -0400

    Amend API deploy to production to use ECS production service

--- hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HealthController.java
+++ hk_api/src/main/java/com/redeaglesoftware/hk_api/controller/HealthController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping
     public ResponseEntity<String> healthcheck() {
-        return ResponseEntity.ok("healthy");
+        return ResponseEntity.ok("Healthy");
     }
 }
